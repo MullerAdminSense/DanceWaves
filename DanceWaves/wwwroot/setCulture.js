7cd356fc6182c97a16a76a687f705512e7c5cdb6
mfmuller70 <SEU_EMAIL@example.com>
2025-11-12 18:07:32 -0300

    issues

--- DanceWaves/wwwroot/setCulture.js
+++ DanceWaves/wwwroot/setCulture.js
@@ -1,13 +1,9 @@
 window.setCulture = function (culture) {
     var expires = new Date();
     expires.setFullYear(expires.getFullYear() + 1);
-    // Remove Secure em desenvolvimento (HTTP) - será adicionado automaticamente em produção (HTTPS)
-    var isSecure = window.location.protocol === 'https:';
     var cookieString = ".AspNetCore.Culture=c=" + culture + "|uic=" + culture + "; path=/; expires=" + expires.toUTCString();
-    if (isSecure) {
-        cookieString += "; Secure";
-    }
+    // Não adiciona Secure em HTTP (IIS Express)
     document.cookie = cookieString;
     console.log("Culture cookie set to:", culture);
-    window.location.href = "/setculture/" + culture;
+    window.location.reload();
 };
